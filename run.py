404dc9f4cc9d9eb22243785bb3be44660d427430
Lukasz Nowicki <nowicki.lukasz93@gmail.com>
2022-03-12 15:46:00 +0000

    Create ships on board

--- run.py
+++ run.py
@@ -1,3 +1,5 @@
+from random import randint
+
 """
 Creates a list of lists in an 8 x 8 pattern to represent a playing board 
 used in the game.
@@ -12,7 +14,7 @@ for i in range(8):
 
 def print_game_board(board):
     """
-    Prints a layout of the board game, with head legend. 
+    Prints a layout of the board game, with head legend.
     """
     print("  A B C D E F G H")
     print("  ===============")
@@ -23,8 +25,21 @@ def print_game_board(board):
 
 
 
-def create_battleships():
-    pass
+def create_battleships(board):
+    """
+    Creates ships on the board randomly using row value and column value.
+    10 ships created and labled with an "@", to show the location
+    """
+    for ship in range(10):
+        x_row = randint(0, 7)
+        y_column = randint(0, 7)
+
+    while board[x_row] == "@" and board[y_column] == "@":
+        x_row = randint(0, 7)
+        y_column = randint(0, 7)
+    
+    board[x_row] = "@"    
+    board[y_column] = "@"
 
 
 def player_guess_input():
@@ -35,5 +50,5 @@ def check_if_hit_or_miss():
     pass
 
 
-
+create_battleships(player_board)
 print_game_board(player_board)
