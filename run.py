174ba0c22dce2db217d9bf492219888aa27d59ec
Lukasz Nowicki <nowicki.lukasz93@gmail.com>
2022-03-12 15:04:06 +0000

    Define basic functions for the game logic

--- run.py
+++ run.py
@@ -8,3 +8,23 @@ for i in range(8):
     player_board.append([" "] * 8)
 
 print(player_board)
+
+
+
+
+
+def print_game_board():
+    pass
+
+
+def create_battleships():
+    pass
+
+
+def player_guess_input():
+    pass
+
+
+def check_if_hit_or_miss():
+    pass
+
